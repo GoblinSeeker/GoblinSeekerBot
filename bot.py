9c2031b5947eb2de4e9bfb56def71211d68b3c99
GoblinSeeker <pieselgeter@gmail.com>
2026-05-13 17:46:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,27 +3,21 @@ import os
 import feedparser
 import random
 
-# Wir laden die 1.0a Keys aus deinen Secrets
-api_key = os.environ['X_CLIENT_ID']
-api_secret = os.environ['X_CLIENT_SECRET']
+client_id = os.environ['X_CLIENT_ID']
+client_secret = os.environ['X_CLIENT_SECRET']
 access_token = os.environ['X_ACCESS_TOKEN']
-access_secret = os.environ['X_REFRESH_TOKEN']
+refresh_token = os.environ['X_REFRESH_TOKEN']
 
-# Authentifizierung strikt nach OAuth 1.0a
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_secret)
-api = tweepy.API(auth)
-
-# Client für v2 Befehle (wie create_tweet) mit 1.0a Daten füttern
 client = tweepy.Client(
-    consumer_key=api_key,
-    consumer_secret=api_secret,
+    consumer_key=client_id,
+    consumer_secret=client_secret,
     access_token=access_token,
-    access_token_secret=access_secret
+    access_token_secret=refresh_token
 )
 
 def search_and_post():
     query = "goblin sightings"
-    rss_url = f"https://news.google.com/rss/search?q={query}&hl=en-US&gl=US&ceid=US:en"
+    rss_url = f"https://news.google.com/rss/search?q={query.replace(' ', '+')}&hl=en-US&gl=US&ceid=US:en"
     feed = feedparser.parse(rss_url)
     
     if feed.entries:
@@ -32,9 +26,9 @@ def search_and_post():
         
         try:
             client.create_tweet(text=tweet_text)
-            print("Erfolg! Der Post ist raus.")
+            print("Success")
         except Exception as e:
-            print(f"Fehler: {e}")
+            print(f"Error: {e}")
 
 if __name__ == "__main__":
     search_and_post()
